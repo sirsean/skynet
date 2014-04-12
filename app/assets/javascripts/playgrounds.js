2dd3207e213bca87f70c1f8b7081f7e6c78ccbcb
Sean Schulte <sirsean@gmail.com>
2014-04-12 16:56:39 -0500

    show picture?

--- app/assets/javascripts/playgrounds.js
+++ app/assets/javascripts/playgrounds.js
@@ -1,11 +1,33 @@
+function showPicture(file) {
+  var show = $("#show-picture");
 
+  // Get window.URL object
+  var URL = window.URL || window.webkitURL;
+   
+  // Create ObjectURL
+  var imgURL = URL.createObjectURL(file);
+   
+  // Set img src to ObjectURL
+  show.src = imgURL;
+   
+  // For performance reasons, revoke used ObjectURLs
+  URL.revokeObjectURL(imgURL);
+}
 
-
-console.log('hi');
+console.log("hi");
+$(function() {
+  $("#take-picture").on("change", function() {
+    console.log("changed");
+    console.log(this.files);
+    var file = this.files[0];
+    $("#filename").text(file.name);
+    showPicture(file);
+  });
+});
 
 function geoSuccess(position) {
   console.log(position);
-  $("#latitude").html(position.coords.latitude);
+  $("#latitude").text(position.coords.latitude);
   $("#longitude").text(position.coords.longitude);
 }
 
