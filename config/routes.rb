af336f89bbd793dbb6349b906cefe2910386438c
Sean Schulte <sirsean@gmail.com>
2014-04-12 16:08:11 -0500

    playground

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Skynet::Application.routes.draw do
+  get "playgrounds/index"
+
+  root "playgrounds#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
