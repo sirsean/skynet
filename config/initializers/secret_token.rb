04529321abceb28bb0eb32b11bdf5df881656a94
Sean Schulte <sirsean@gmail.com>
2014-04-12 14:34:37 -0500

    Initial. (Re-done.)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Skynet::Application.config.secret_key_base = '82adfd3605ae4ee6a2ffc354eefb859ee7e0818544ea5521878a645c592c72b96687bab1dbc30ef2458e7aa1df163cdd56c70bd7a0901b130bbdbd3661e39beb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Skynet::Application.config.session_store :cookie_store, key: '_skynet_session'
