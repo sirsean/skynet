00b3e9163e493c3e0c8bf451181475c1ddcf00a9
Sean Schulte <sirsean@gmail.com>
2014-04-12 16:16:36 -0500

    playground

--- app/assets/javascripts/playgrounds.js
+++ app/assets/javascripts/playgrounds.js
@@ -2,3 +2,17 @@
 
 
 console.log('hi');
+
+function geoSuccess(position) {
+  console.log(position);
+  $("#latitude").html(position.coords.latitude);
+  $("#longitude").text(position.coords.longitude);
+}
+
+function geoError(msg) {
+  console.log(msg);
+}
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+}
