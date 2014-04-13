add55c9bdd7d8dd406cb95075b59bea678559429
Sean Schulte <sirsean@gmail.com>
2014-04-13 00:37:45 -0500

    Hopefully not throwing away uploads each deploy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,6 +28,7 @@ set :deploy_to, '/home/skynet/skynet'
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, fetch(:linked_dirs) + %w{public/uploads}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
