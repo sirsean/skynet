ada23329ff14f0f00291d28fa9cb100ab7134a91
Sean Schulte <sirsean@gmail.com>
2014-04-14 21:44:47 -0500

    trying to get unicorn staging going

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,6 +1,7 @@
 root = "/home/skynet/skynet/current"
 working_directory root
 pid "/var/skynet/unicorn.pid"
+set :unicorn_pid, "/var/skynet/unicorn.pid"
 listen "/var/skynet/unicorn.sock"
 stderr_path "/var/skynet/unicorn.log"
 stdout_path "/var/skynet/unicorn.log"
