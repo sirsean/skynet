e5259d1cc1d0ab7af61a903306a43e2bf3f7ad9c
Sean Schulte <sirsean@gmail.com>
2014-04-18 11:06:51 -0500

    Refactoring some Javascript.

---
+++ app/assets/javascripts/factors.js
@@ -0,0 +1,19 @@
+var Factors = new function() {
+  this.known = [
+    [53, 113, 181],
+    [90, 136, 189],
+    [104, 144, 199],
+    [126, 158, 206],
+    [149, 177, 216],
+    [177, 196, 225]
+  ];
+
+  this.knownSummaries = [
+    { hero: "Flawless", left: "Perfect", middle: "Perfect", right: "None" },
+    { hero: "Beautiful", left: "Lovely", middle: "Great", right: "Very Low" },
+    { hero: "Clear Blue", left: "Good", middle: "Fair", right: "Low" },
+    { hero: "Pale Sky", left: "Pale", middle: "Fair", right: "Moderate" },
+    { hero: "Hazy Sky", left: "Pale", middle: "Hazy", right: "High" },
+    { hero: "Milky Sky", left: "Poor", middle: "Poor", right: "Very High" }
+  ];
+};
